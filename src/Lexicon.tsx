6eb2c1047bae95225b8f7825daeb2675a68b3f1c
johannes l <crushy99HD@gmail.com>
2023-09-19 00:27:18 +0200

    add some colors and basic css

--- src/Lexicon.tsx
+++ src/Lexicon.tsx
@@ -5,18 +5,44 @@ export const Lexicon = () => {
 
     return <div
         id={"lexicon-main"}
-        css={testCss}
+        css={mainCss}
     >
         {EnglishLexicon.map((entry) => {
-            return <div>
-                <h1>{entry.name}</h1>
-                <h2>{entry.secondaryNames}</h2>
-                <text>{entry.entry}</text>
+            return <div
+                css={entryOuterCss}
+            >
+                <div
+                    css={entryInnerCss}
+                >
+                    <h1>{entry.name}</h1>
+                    <h4>{entry.secondaryNames}</h4>
+                    <text>{entry.entry}</text>
+                </div>
             </div>;
         })}
     </div>
 }
 
-const testCss = css`
-    background-color: indianred;
+const mainCss = css`
+    @import url('https://fonts.googleapis.com/css2?family=Lexend:wght@100;200;300;400;500;600;700;800;900&display=swap');
+    font-family: 'Lexend', sans-serif;
+  
+    display: flex;
+    flex-direction: column;
+    gap: 48px;
+  
+    background-color: #595959;
+`;
+
+const entryOuterCss = css`
+    display: flex;
+    justify-content: center;
+    width: 100%;
+`;
+
+const entryInnerCss = css`
+    width: 50%;
+    padding: 24px;
+    border-radius: 24px;
+    background-color: #cccccc;
 `;
