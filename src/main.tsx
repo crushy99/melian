66e4765736aafb1359728b2406caea7b102b58f1
johannes l <crushy99HD@gmail.com>
2023-09-19 16:17:27 +0200

    change react root to element with id "root"

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,6 @@ import { Lexicon } from './Lexicon.tsx'
 // just started this project and this is already fucked up somehow...
 document.body.style.margin = "-22px 0px 0px 0px";
 
-ReactDOM.createRoot(document.body).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
     <Lexicon />
 )
